a9955451cad7dbf018bc30fce4ff8e836d3a3526
tamil12345-spec <tamilarasi3086@gmail.com>
2026-04-06 15:28:25 +0530

    initial

--- routes/payment.js
+++ routes/payment.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe');
 const { protect } = require('../middleware/auth');
 const Order = require('../models/Order');
 const router = express.Router();
