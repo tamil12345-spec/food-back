a4fe7b09e10746a1186c60ff37064a8c7427af8c
tamilarasi <tamilarasi@gmail.com>
2026-05-01 06:49:40 +0530

    ini

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB first
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
